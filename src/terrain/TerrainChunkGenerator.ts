b7999f2414551d55338ca61df59419289fff19c2
gionny96 <gionathas@gmail.com>
2023-02-10 22:34:12 +0100

    optimized noise computation

--- src/terrain/TerrainChunkGenerator.ts
+++ src/terrain/TerrainChunkGenerator.ts
@@ -11,12 +11,25 @@ import { Coordinate } from "../utils/helpers";
 import { BlockType } from "./Block";
 import Chunk from "./Chunk";
 
+/**
+ * //TODO optimization: instead of creating an heightmap for each chunk,
+ * we can create a bigger heightmap shared by all chunks.
+ * In this way we calculate the heightMap for the entire terrain only one time.
+ * The heightMap will be shared by all workers, such that they will not need to compute
+ * the heightMap at first pass.
+ *
+ * Possible downside is that the heightMap should be calculated by the main thread
+ *  (nested loop that iterate over the x and z plane), whereas with the current implementation
+ *  workers are calculating (and caching) the heightMap by themself but wasting computation
+ */
 export default class TerrainChunkGenerator {
   private noise: NoiseFunction2D;
+  private heightMap: Record<string, number>;
 
   // TODO take in input the seed value (in order to be shared by the workers)
   constructor() {
     this.noise = createNoise2D(alea("seed")); //FIXME add seed
+    this.heightMap = {};
   }
 
   fillTerrain(chunk: Chunk, startX: number, startY: number, startZ: number) {
@@ -30,16 +43,31 @@ export default class TerrainChunkGenerator {
     for (let y = startY; y < chunkHeight; y++) {
       for (let z = startZ; z < endZ; z++) {
         for (let x = startX; x < endX; x++) {
-          this.generateBlock(chunk, { x, y, z });
+          const surfaceHeight = this.generateSurfaceHeight(x, z);
+          this.generateBlock(chunk, { x, y, z }, surfaceHeight);
         }
       }
     }
   }
 
-  private generateBlock(chunk: Chunk, { x, y, z }: Coordinate) {
-    const surfaceHeight =
-      BASE_HEIGHT + this.noise(x / NOISE_SCALE, z / NOISE_SCALE) * HILL_OFFSET;
+  private generateSurfaceHeight(x: number, z: number) {
+    const key = `${x},${z}`;
 
+    if (this.heightMap[key] != null) {
+      return this.heightMap[key];
+    }
+
+    const value =
+      BASE_HEIGHT + this.noise(x / NOISE_SCALE, z / NOISE_SCALE) * HILL_OFFSET;
+    this.heightMap[key] = value;
+    return value;
+  }
+
+  private generateBlock(
+    chunk: Chunk,
+    { x, y, z }: Coordinate,
+    surfaceHeight: number
+  ) {
     let blockType: BlockType;
     if (y < surfaceHeight) {
       blockType = this.generateBlockBelowSurface(y, surfaceHeight);
@@ -47,9 +75,8 @@ export default class TerrainChunkGenerator {
       blockType = this.generateBlockAboveSurface(y, surfaceHeight);
     }
 
-    if (blockType) {
-      chunk.setBlock({ x, y, z }, blockType);
-    }
+    // add the block inside the chunk
+    chunk.setBlock({ x, y, z }, blockType);
   }
 
   private generateBlockBelowSurface(y: number, surfaceHeight: number) {
