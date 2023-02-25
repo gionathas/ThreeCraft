0c6741d109bd419f20bb32b362401f32978605c0
gionny96 <gionathas@gmail.com>
2023-02-25 12:27:11 +0100

    remove commented code

--- src/maps/tree/TreeMap.ts
+++ src/maps/tree/TreeMap.ts
@@ -31,64 +31,9 @@ export default class TreeMap extends Abstract2DMap {
       return false;
     }
 
+    // get the surface height at the trunk
     return true;
-
-    // if (tree) {
-    //   return true;
-    // const trunkStartY = this.heightMap.getSurfaceHeightAt(trunkX, trunkZ);
-    // const trunkEndY = trunkStartY + Tree.TRUNK_HEIGHT;
-
-    // const isTreeLeaves = isInRange(y, trunkEndY - 1, trunkEndY + 1);
-
-    // if (isTreeLeaves) {
-    //   const trunkPosition = new THREE.Vector3(trunkX, trunkEndY, trunkZ);
-    //   const leavePosition = new THREE.Vector3(x, y, z);
-
-    //   const dist = leavePosition.distanceToSquared(trunkPosition);
-
-    //   return true;
-
-    //   // return dist > 1 ? Math.random() > 0.4 : true;
-    // }
-    // }
-
-    // return false;
   }
-
-  /**
-   * If the leaf is near a tree, it return the y level of the leave compared to the trunk end
-   */
-  // private findTreeTrunkForLeafAt(x: number, y: number, z: number) {
-  //   const treeRadius = Tree.RADIUS;
-
-  //   for (let dx = -treeRadius; dx <= treeRadius; dx++) {
-  //     for (let dz = -treeRadius; dz <= treeRadius; dz++) {
-  //       const nearbyX = x + dx;
-  //       const nearbyZ = z + dz;
-
-  //       const isTreeTrunk =
-  //         this.getTreeMapValueAt(nearbyX, nearbyZ) === TreeMapValue.TRUNK;
-
-  //       if (isTreeTrunk) {
-  //         const trunkStartY = this.heightMap.getSurfaceHeightAt(
-  //           nearbyX,
-  //           nearbyZ
-  //         );
-  //         const trunkEndY = trunkStartY + Tree.TRUNK_HEIGHT;
-
-  //         // detect in which y level the leave is
-  //         const leaveYLevel = Math.abs(y - trunkEndY);
-
-  //         // if the leave is in an acceptable y distance from the trunkEnd
-  //         if (leaveYLevel < 3) {
-  //           return leaveYLevel;
-  //         }
-  //       }
-  //     }
-  //   }
-
-  //   return null;
-  // }
 
   shouldSpawnTreeTrunkAt(x: number, y: number, z: number, surfaceY: number) {
     if (y < surfaceY || y > surfaceY + Tree.TRUNK_HEIGHT) {
