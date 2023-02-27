42be02de11ffe17a26c2174976535851c9c0ffd7
gionny96 <gionathas@gmail.com>
2023-02-27 11:18:06 +0100

    add readonly attribute to world constants

--- src/terrain/World.ts
+++ src/terrain/World.ts
@@ -2,28 +2,29 @@ import EnvVars from "../config/EnvVars";
 import { Chunk } from "./chunk";
 
 export default class World {
-  static CONTINENTALNESS_NOISE_SCALE = 10000;
-  static EROSION_NOISE_SCALE = 1024;
-  static PV_BASE_SCALE = 180;
+  static readonly CONTINENTALNESS_NOISE_SCALE = 10000;
+  static readonly EROSION_NOISE_SCALE = 1024;
+  static readonly PV_BASE_SCALE = 180;
 
-  static MAX_WORLD_HEIGHT =
+  static readonly MAX_WORLD_HEIGHT =
     EnvVars.TOP_RENDER_DISTANCE_IN_CHUNKS * Chunk.HEIGHT;
-  static MIN_WORLD_HEIGHT =
+  static readonly MIN_WORLD_HEIGHT =
     EnvVars.BOTTOM_RENDER_DISTANCE_IN_CHUNKS * -Chunk.HEIGHT;
 
-  static MIN_EROSION = 0;
-  static MAX_EROSION = 45;
+  static readonly MIN_EROSION = 0;
+  static readonly MAX_EROSION = 45;
 
-  static CONTINENTALNESS_MIN_HEIGHT = 0;
-  static CONTINENTALNESS_MAX_HEIGHT = this.CONTINENTALNESS_MIN_HEIGHT + 50;
+  static readonly CONTINENTALNESS_MIN_HEIGHT = 0;
+  static readonly CONTINENTALNESS_MAX_HEIGHT =
+    this.CONTINENTALNESS_MIN_HEIGHT + 50;
 
   // WARN must be less than MAX_WORLD_HEIGHT
-  static MAX_SURFACE_HEIGHT =
+  static readonly MAX_SURFACE_HEIGHT =
     this.CONTINENTALNESS_MAX_HEIGHT + this.MAX_EROSION;
   // WARN must be greater than MIN_WORLD_HEIGHT
-  static MIN_SURFACE_HEIGHT =
+  static readonly MIN_SURFACE_HEIGHT =
     this.CONTINENTALNESS_MIN_HEIGHT - this.MAX_EROSION;
 
-  static SEA_LEVEL = this.CONTINENTALNESS_MIN_HEIGHT + 10;
-  static CLOUD_LEVEL = this.MAX_SURFACE_HEIGHT - 10;
+  static readonly SEA_LEVEL = this.CONTINENTALNESS_MIN_HEIGHT + 10;
+  static readonly CLOUD_LEVEL = this.MAX_SURFACE_HEIGHT - 10;
 }
