11aa0062d0736925776e819152185031b4503a39
gionny96 <gionathas@gmail.com>
2023-03-18 23:14:50 +0100

    add github link and custom app icon

--- src/ui/MainMenu.ts
+++ src/ui/MainMenu.ts
@@ -1,5 +1,7 @@
 export default class MainMenu {
   private mainMenu: HTMLElement;
+
+  private githubIcon: HTMLElement;
   private overlay: HTMLElement;
   private startBtn: HTMLElement;
 
@@ -7,16 +9,19 @@ export default class MainMenu {
     this.mainMenu = document.getElementById("main-menu")!;
     this.overlay = document.getElementById("overlay")!;
     this.startBtn = document.getElementById("play-btn")!;
+    this.githubIcon = document.getElementById("github-link-icon")!;
   }
 
   show() {
     this.mainMenu.style.display = "block";
     this.overlay.style.display = "block";
+    this.githubIcon.style.display = "block";
   }
 
   hide() {
     this.mainMenu.style.display = "none";
     this.overlay.style.display = "none";
+    this.githubIcon.style.display = "none";
   }
 
   setOnPlayWorld(callback: () => void) {
