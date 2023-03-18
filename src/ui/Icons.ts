e41ab18d9638f7d3d754ff6111ec8cfd7429fb75
gionny96 <gionathas@gmail.com>
2023-03-18 23:58:13 +0100

    imported new icon asset

--- src/ui/Icons.ts
+++ src/ui/Icons.ts
@@ -3,7 +3,7 @@ import { BlockRegistry, BlockType } from "../terrain/block";
 export default class Icons {
   private static readonly ICON_SIZE = 32;
 
-  private static readonly URL_PATH = "src/assets/textures/block-icons.png";
+  private static readonly URL_PATH = "src/assets/icons.png";
 
   static getBlockIconUrlPath(block: BlockType) {
     return Icons.URL_PATH;
